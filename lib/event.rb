161060d5111e2df2385fcc7ee3c2b7b079c6308e
jenmccarthy <jennifernward@gmail.com>
2014-08-22 13:58:10 -0700

    select events by date ranges

--- lib/event.rb
+++ lib/event.rb
@@ -1,11 +1,37 @@
 class Event < ActiveRecord::Base
 
   def self.future_events
-    Event.where ("start_date > ?", Time.now.strftime("%Y-%m-%d"))
+    Event.where("start_date > ?", Time.now.strftime("%Y-%m-%d"))
   end
 
-  # def self.future_events
-  #   Event.where(start_date: (Time.now.stf))
-  # end
+  def self.current_month
+    self.all.select { |event| event.start_date.month == Time.now.month }
+  end
+
+
+  def self.current_day
+    self.all.select do |event|
+      if event.start_date.month == Time.now.month && event.end_date.month == Time.now.month
+        (event.start_date.day == Time.now.day) && (event.end_date.day == Time.now.day)
+      end
+    end
+  end
+
+  def self.current_week
+    self.all.select do |event|
+      if event.start_date.month == Time.now.month && event.end_date.month == Time.now.month
+        start_day = event.start_date.to_date
+        end_day = event.end_date.to_date
+        day_of_week = start_day.wday
+        if day_of_week == 0
+          event.start_date.day + 6
+          # print event from this date plus 6 for end_date
+
+          end
+        binding.pry
+      end
+    end
+  end
+
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -8,4 +8,34 @@ describe Event do
       expect(Event.future_events).to eq [event_1]
     end
   end
+
+  describe '.current_month' do
+    it 'lists all events within the current month' do
+      event_1 = Event.create({start_date: '2014-08-25', end_date: '2014-08-25'})
+      event_2 = Event.create({start_date: '2014-08-30', end_date: '2014-08-31'})
+      event_3 = Event.create({start_date: '2014-12-30', end_date: '2014-12-31'})
+      expect(Event.current_month).to eq [event_1, event_2]
+    end
+  end
+
+  describe '.current_day' do
+    it 'lists all events within the current day' do
+      event_1 = Event.create({start_date: '2014-08-22', end_date: '2014-08-22'})
+      event_2 = Event.create({start_date: '2014-08-22', end_date: '2014-08-22'})
+      event_3 = Event.create({start_date: '2014-08-22', end_date: '2014-08-26'})
+      event_4 = Event.create({start_date: '2014-12-30', end_date: '2014-12-31'})
+      expect(Event.current_day).to eq [event_1, event_2]
+    end
+  end
+
+  describe '.current_week' do
+    it 'lists all events within the current week' do
+      event_1 = Event.create({start_date: '2014-08-17', end_date: '2014-08-23'})
+      event_2 = Event.create({start_date: '2014-08-20', end_date: '2014-08-22'})
+      event_3 = Event.create({start_date: '2014-08-22', end_date: '2014-08-26'})
+      event_4 = Event.create({start_date: '2014-12-30', end_date: '2014-12-31'})
+      expect(Event.current_week).to eq [event_1, event_2]
+    end
+  end
+
 end
